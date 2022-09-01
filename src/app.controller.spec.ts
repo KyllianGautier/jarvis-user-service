854f44535c2c81e14ec3681a8ad2428511d15823
Kyllian <kyllian.gt@hotmail.fr>
2022-09-02 01:34:51 +0200

    Setup hello world

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello, I am Jarvis"', () => {
+      expect(appController.getHello()).toBe('Hello, I am Jarvis');
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): { answer: string } {
+    return { answer: this.appService.getHello() };
   }
 }
